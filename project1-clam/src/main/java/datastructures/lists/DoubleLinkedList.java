d84547cf53fb0bb2e89d82c0795296f18db817c9
kim-913 <77228202+kim-913@users.noreply.github.com>
2021-02-09 19:15:31 -0800

    Add files via upload

---
+++ project1-clam/src/main/java/datastructures/lists/DoubleLinkedList.java
@@ -0,0 +1,264 @@
+package datastructures.lists;
+
+import datastructures.EmptyContainerException;
+import java.util.Objects;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Note: For more info on the expected behavior of your methods:
+ * @see IList
+ * (You should be able to control/command+click "IList" above to open the file from IntelliJ.)
+ */
+public class DoubleLinkedList<T> implements IList<T> {
+    /*
+    Warning:
+    You may not rename these fields or change their types.
+    We will be inspecting these in our secret tests.
+    You also may not add any additional fields.
+
+    Note: The fields below intentionally omit the "private" keyword. By leaving off a specific
+    access modifier like "public" or "private" they become package-private, which means anything in
+    the same package can access them. Since our tests are in the same package, they will be able
+    to test these properties directly.
+     */
+    Node<T> front;
+    Node<T> back;
+    int size;
+
+    public DoubleLinkedList() {
+        this.front = null;
+        this.back = null;
+        this.size = 0;
+    }
+
+    private Node<T> position(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        if (index > size / 2) {
+            Node<T> current = back;
+            for (int i = size - 1; i > index; i--) {
+                current = current.prev;
+            }
+            return current;
+        } else {
+            Node<T> current = front;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+            return current;
+        }
+    }
+    @Override
+    public void add(T item) {
+        if (size == 0) {
+            front = new Node<T>(item);
+            back = front;
+        } else {
+            back.next = new Node<T>(back, item, null);
+            back = back.next;
+        }
+        size++;
+    }
+
+    @Override
+    public T remove() {
+        if (size == 0) {
+            throw new EmptyContainerException();
+        }
+        T result = back.data;
+        if (size == 1) {
+            front = null;
+            back = null;
+        } else {
+            back = back.prev;
+            back.next = null;
+        }
+        size--;
+        return result;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> current = position(index);
+        return current.data;
+    }
+
+    @Override
+    public T set(int index, T item) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> current = position(index);
+
+        if (index == 0) {
+            front = new Node<T>(null, item, front.next);
+            if (front.next != null) {
+                front.next.prev = front;
+            } else {
+                back = front;
+            }
+        } else {
+            current.prev.next = new Node<T>(current.prev, item, current.next);
+            if (current.next != null) {
+                current.next.prev = current.prev.next;
+            } else {
+                back = current.prev.next;
+            }
+        }
+        return current.data;
+    }
+
+    @Override
+    public void insert(int index, T item) {
+        if (index < 0 || index >= size + 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            front = new Node<T>(null, item, front);
+            if (front.next != null) {
+                front.next.prev = front;
+            } else {
+                back = front;
+            }
+        } else {
+            Node<T> current = position(index);
+            if (current != null) {
+                current.prev.next = new Node<T>(current.prev, item, current);
+                current.prev = current.prev.next;
+            } else {
+                back.next = new Node<T>(back, item, null);
+                back = back.next;
+            }
+        }
+        size++;
+    }
+
+    @Override
+    public T delete(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> current = position(index);
+        if (index == 0) {
+            front = front.next;
+            if (front == null) {
+                back = null;
+            } else {
+                front.prev = null;
+            }
+        } else if (current.next == null) {
+            back = back.prev;
+            back.next = null;
+        } else {
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+        }
+        size--;
+        return current.data;
+    }
+
+    @Override
+    public int indexOf(T item) {
+        Node<T> current = front;
+        int index = 0;
+        while (current != null) {
+            if (Objects.equals(current.data, item)) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean contains(T other) {
+        return indexOf(other) != -1;
+    }
+
+    @Override
+    public String toString() {
+        // return super.toString();
+
+        /*
+        After you've implemented the iterator, comment out the line above and uncomment the line
+        below to get a better string representation for objects in assertion errors and in the
+        debugger.
+        */
+
+        return IList.toString(this);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        /*
+        Note: we have provided a part of the implementation of an iterator for you. You should
+        complete the methods stubs in the DoubleLinkedListIterator inner class at the bottom of
+        this file. You do not need to change this method.
+        */
+        return new DoubleLinkedListIterator<>(this.front);
+    }
+
+    static class Node<E> {
+        // You may not change the fields in this class or add any new fields.
+        final E data;
+        Node<E> prev;
+        Node<E> next;
+
+        Node(Node<E> prev, E data, Node<E> next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        Node(E data) {
+            this(null, data, null);
+        }
+
+        // Feel free to add additional constructors or methods to this class.
+    }
+
+    private static class DoubleLinkedListIterator<T> implements Iterator<T> {
+        // You should not need to change this field, or add any new fields.
+        private Node<T> next;
+
+        public DoubleLinkedListIterator(Node<T> front) {
+            // You do not need to make any changes to this constructor.
+            this.next = front;
+        }
+
+        /**
+         * Returns `true` if the iterator still has elements to look at;
+         * returns `false` otherwise.
+         */
+        public boolean hasNext() {
+            return next != null;
+        }
+
+        /**
+         * Returns the next item in the iteration and internally updates the
+         * iterator to advance one element forward.
+         *
+         * @throws NoSuchElementException if we have reached the end of the iteration and
+         *         there are no more elements to look at.
+         */
+        public T next() {
+            if (next == null) {
+                throw new NoSuchElementException();
+            }
+            Node<T> temp = next;
+            next = next.next;
+            return temp.data;
+        }
+    }
+}
